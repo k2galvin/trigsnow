6cd6fd32e6e1269f9cf65fedf529fa1ae35d52ee
k2galvin <kieran@pagerduty.com>
2017-09-14 21:15:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -202,13 +202,14 @@ function addTag(token, incident){
 }
 
 app.post('/addtags', function(req, res) {
-
+	
 	
 	var incident = req.body.messages[0].incident;
 	//var service = req.body.messages[0].incident.service.name
 	var token = req.query.token;
 	var event = req.body.messages[0].event;
 	var incidentURL = req.body.messages[0].incident.self;
+	addTag(token, incident);
 	console.log("calling add tag");
 
 
