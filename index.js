c0791fae4c3db9759eb84192068882f6780412dd
k2galvin <kieran@pagerduty.com>
2017-09-22 22:27:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ function addTag(token, incident,tag){
 	});
 }
 
-app.post('/addtags_CXO', function(req, res) {
+app.post('/addtags_cxo', function(req, res) {
 	//add the tag for CXO w/ID PWR861O
 	
 	console.log("calling add tag");
